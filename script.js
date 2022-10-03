f9551c9a502b2bc7ae326c12225d6dfa91bfbfba
AndrewwBC <andrew-hot2011@hotmail.com>
2022-10-03 00:53:50 -0300

    Responsivo

--- script.js
+++ script.js
@@ -12,29 +12,30 @@ else if(window.location.href === 'http://127.0.0.1:5500/inner-bikes.html') {
 } */
 
 
-
 let isOpen = false;
-const questionsCont = document.getElementById('questions-1');
+const texto = document.getElementById('texto-add');
+const botao = document.getElementById('questions-1-btn');
+const recebe = document.getElementById('questions-1');
+const original = recebe.innerHTML;
 
+function ativar(){
 
-
-function ativar() {
     if(isOpen === false){
         isOpen = true;
-        document.getElementById('questions-1-btn').style.transform = 'rotate(180deg)';
-        document.getElementById('questions-1').innerHTML =  document.getElementById('questions-1').innerHTML + '<div>' +
-        '<p class="font-2-s">Aceitamos cartões de crédito com as bandeiras: Visa, Mastercard, ELO e American Express. Para pagamentos à vista também aceitarmos PIX e Boleto através do PagSeguro.</p>' +
-       '</div>';
+        botao.style.transform = 'rotate(180deg)';
+        recebe.innerHTML =  recebe.innerHTML + texto.innerHTML;  
         
- }
-    else {
-        isOpen = false;
-        document.getElementById('questions-1').innerHTML =  document.getElementById('questions-1').innerHTML;
-        document.getElementById('questions-1-btn').style.transform = 'rotate(180deg)';
      }
-     
+    else if(isOpen === true){
+        recebe.innerHTML =  original;
+        isOpen = false;
+        console.log('false');
+        
+    }
 }
 
-document.getElementById('questions-1-btn').addEventListener("click", ativar);
+
+botao.addEventListener('click', ativar);
 
 
+// orçamento 
